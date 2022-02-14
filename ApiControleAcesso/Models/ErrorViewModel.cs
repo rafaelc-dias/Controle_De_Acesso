b099fc0aa02f5e94bd2e7a6fe31bfd4e3ca727f1
Rafael Dias <rafael.dias@teadit.com>
2022-02-14 08:41:11 -0300

    Commit Inicial implementação API

---
+++ ApiControleAcesso/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ApiControleAcesso.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
