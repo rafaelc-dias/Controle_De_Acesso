37f1861ebbe761af4e24db20e8d42a0d4cba0bd7
Rafael Dias <rafael.dias@teadit.com>
2022-03-24 09:42:38 -0300

    Controllers

---
+++ ControleAcesso.API/Controllers/EntradaFuncionarioController.cs
@@ -0,0 +1,107 @@
+﻿using ControleAcesso.Class;
+using ControleAcesso.Domain.DTO;
+using ControleAcesso.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleAcesso.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntradaFuncionarioController : ControllerBase
+    {
+        private readonly IEntradaFuncionarioServico _entradafuncionarioServico;
+
+        public EntradaFuncionarioController(IEntradaFuncionarioServico entradafuncionarioServico)
+        {
+            _entradafuncionarioServico = entradafuncionarioServico;
+        }
+
+        [HttpGet]
+        [Route("listar")]
+        public async Task<IActionResult> listar()
+        {
+            try
+            {
+               
+                var _response = await _entradafuncionarioServico.Listar();
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        [Route("cadastrar")]
+        public async Task<IActionResult> AdiconarEntradaFuncionarios(EntradaFuncionarioDTO entradaFuncionarioDTO)
+        {
+            try
+            {
+                if (await _entradafuncionarioServico.Cadastrar(entradaFuncionarioDTO) > 0)
+                {
+                    return Ok("Cadastrado !!!!");
+                }
+                else
+                {
+                    return NoContent();
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPut]
+        [Route("alterarstatus/{movimentoId}")]
+        public async Task<IActionResult> AlterarStatusfechado(Guid movimentoId)
+        {
+            try
+            {
+                await _entradafuncionarioServico.AlterarStatusFechado(movimentoId);
+                return Ok("Atualizado !!!!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        [HttpPut]
+        [Route("adicionadatasaida/{movimentoId}")]
+        public async Task<IActionResult> AdicionaDatdaSaida(Guid movimentoId)
+        {
+            try
+            {
+                await _entradafuncionarioServico.AdicionaDataSaida(movimentoId);
+                return Ok("Atualizado !!!!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        [HttpDelete]
+        [Route("excluir/{id}")]
+        public async Task<IActionResult> RemoverEntradaFuncionario(Guid id)
+        {
+            try
+            {
+                await _entradafuncionarioServico.Excluir(id);
+                return Ok("Exclusão");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+    }
+}
---
+++ ControleAcesso.API/Controllers/VeiculoController.cs
@@ -0,0 +1,93 @@
+﻿using ControleAcesso.Class;
+using ControleAcesso.Domain.DTO;
+using ControleAcesso.Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleAcesso.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculoController : ControllerBase
+    {
+        private readonly IVeiculoServico _veiculosServico;
+
+        public VeiculoController(IVeiculoServico veiculosServico)
+        {
+            _veiculosServico = veiculosServico;
+        }
+
+        [HttpGet]
+        [Route("listar")]
+        public async Task<IActionResult> listar()
+        {
+            try
+            {
+                return Ok(await _veiculosServico.Listar());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpGet]
+        [Route("pesquisar/{id}")]
+        public async Task<IActionResult> RetonarVeiculos(Guid id)
+        {
+            try
+            {
+                return Ok(await _veiculosServico.Pesquisar(id));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        [Route("cadastrar")]
+        public async Task<IActionResult> AdiconarVeiculos(VeiculoDTO veiculoDTO)
+        {
+            try
+            {
+                await _veiculosServico.Cadastrar(veiculoDTO.ConverteVeiculo());
+                return Ok("Cadastrado !!!!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPut]
+        [Route("atualizar")]
+        public async Task<IActionResult> AlterarVeiculos(VeiculoDTO veiculoDTO)
+        {
+            try
+            {
+                await _veiculosServico.Atualizar(veiculoDTO.ConverteVeiculo());
+                return Ok("Atualizado !!!!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpDelete]
+        [Route("excluir/{id}")]
+        public async Task<IActionResult> RemoverVeiculos(Guid id)
+        {
+            try
+            {
+                await _veiculosServico.Excluir(id);
+                return Ok("Exclusão");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
